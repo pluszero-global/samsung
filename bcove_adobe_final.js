419e3d938e272e95dd9783e8d65f63f047ca1296
Pluszero <77051224+pluszero-global@users.noreply.github.com>
2023-11-20 16:47:47 +0900

    Update bcove_adobe_final.js

--- bcove_adobe_final.js
+++ bcove_adobe_final.js
@@ -1,5 +1,7 @@
- var aa_aaa_aaaaaaaaasdf = this;//videojs.getPlayer("#vjs_video_3_html5_api");
+videojs.registerPlugin('adobeplugin', function() {
+var aa_aaa_aaaaaaaaasdf = this;//videojs.getPlayer("#vjs_video_3_html5_api");
 
     aa_aaa_aaaaaaaaasdf.on("play", function(){
      parent.postMessage("brcove", "*");
     });
+});
