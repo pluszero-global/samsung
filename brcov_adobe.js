8b8fe846951f8778184c9e9376db96d3de27146f
Pluszero <77051224+pluszero-global@users.noreply.github.com>
2023-11-20 16:26:08 +0900

    Update brcov_adobe.js

--- brcov_adobe.js
+++ brcov_adobe.js
@@ -1,7 +1,7 @@
-
-  var aa_aaa_aaaaaaaaasdf = videojs.getPlayer("#vjs_video_3_html5_api");
+videojs.registerPlugin('adobeplugin', function() {
+  var aa_aaa_aaaaaaaaasdf = this;//videojs.getPlayer("#vjs_video_3_html5_api");
 
     aa_aaa_aaaaaaaaasdf.on("play", function(){
-      console.log("ADSFASD");
-        //parent.postMessage("brcove", "*");
+     console.log("ASDF");
     });
+});
