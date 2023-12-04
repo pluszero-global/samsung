dfea52a88a2ef064bd76f4d68abc4f79541ad051
Pluszero <77051224+pluszero-global@users.noreply.github.com>
2023-12-04 14:23:45 +0900

    Update adobe_bcove_final.js

--- adobe_bcove_final.js
+++ adobe_bcove_final.js
@@ -1,8 +1,10 @@
 videojs.registerPlugin('adobeplugin', function() {
 var aa_aaa_aaaaaaaaasdf = this;//videojs.getPlayer("#vjs_video_3_html5_api");
 
+    media_id = this['mediainfo']['id'];
+    message = "brcove_" + media_id;
     aa_aaa_aaaaaaaaasdf.on("play", function(){
-     parent.postMessage("brcove", "*");
+     parent.postMessage(message, "*");
         console.log("testestest");
     });
 });
