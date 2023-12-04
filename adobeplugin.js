b72c74e9035c0f3f3a745dd92470dd003ab3ae20
Pluszero <77051224+pluszero-global@users.noreply.github.com>
2023-12-04 14:37:05 +0900

    Create adobeplugin.js

---
+++ adobeplugin.js
@@ -0,0 +1,11 @@
+videojs.registerPlugin('adobeplugin', function() {
+var aa_aaa_aaaaaaaaasdf = this;//videojs.getPlayer("#vjs_video_3_html5_api");
+
+    media_id = this['mediainfo']['id'];
+    message = "brcove_" + media_id;
+    aa_aaa_aaaaaaaaasdf.on("play", function(){
+     parent.postMessage(message, "*");
+      console.log(message);
+      console.log(media_id);
+    });
+});
